72cd168a9f4e0e0a7a79522851c82a499d53f5e0
Jan Tříska <jan.triska@soskolin.cz>
2026-03-19 09:22:50 +0100

    Trida klient - pridana

---
+++ Banka/Banka/Klient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banka
+{
+    public class Klient
+    {
+        public string Jmeno { get; set; }
+        public string Prijmeni { get; set; }
+        public string UzivatelskeJmeno { get; set; }
+        public string Heslo { get; set; }
+        
+        public Klient(string jmeno, string prijmeni, string uzivatelskeJmeno, string heslo)
+        {
+            Jmeno = jmeno;
+            Prijmeni = prijmeni;
+            UzivatelskeJmeno = uzivatelskeJmeno;
+            Heslo = heslo;
+        }
+        public override string ToString()
+        {
+            return $"{Prijmeni} {Jmeno} - {UzivatelskeJmeno}";
+        }
+    }
+}
