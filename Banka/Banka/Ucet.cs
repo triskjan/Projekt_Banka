c09efa897c8f65a8b19e16a5495c5ebf02458a18
Jan Tříska <jan.triska@soskolin.cz>
2026-03-19 09:25:39 +0100

    Trida Ucet - pridana

---
+++ Banka/Banka/Ucet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banka
+{
+    public class Ucet
+    {
+        /*
+         * a.	Vlastnosti
+            i.	CisloUctu
+            ii.	Zustatek
+            b.	Metody
+            i.	VlozPenize()
+            ii.	VyberPenize()
+        */
+        public string CisloUctu { get; set; }
+        public decimal Zustatek { get; set; }
+
+        public Ucet(string cisloUctu)
+        {
+            CisloUctu = cisloUctu;
+        }
+
+        public Ucet(string cisloUctu, decimal zustatek) : this(cisloUctu)
+        {
+            Zustatek = zustatek;
+        }
+
+        public void VlozPenize(decimal castka)
+        {
+
+        }
+
+        public void VyberPenize(decimal castka)
+        {
+
+        }
+    }
+}
